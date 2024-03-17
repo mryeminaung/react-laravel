8fd6efb9003fb0a166c625c464db4f3ab89104cf
mryeminaung <yeminaung013@gmail.com>
2024-03-17 23:39:48 +0630

    post api testing

---
+++ backend/database/factories/PostFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
+ */
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => $this->faker->sentence,
+            'intro' => $this->faker->sentence(3),
+            'slug' => $this->faker->slug(4),
+            'body' => $this->faker->paragraph(),
+            'user_id' => rand(1, 10),
+        ];
+    }
+}
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\post\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+/* Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+ */
+
+/* API For Users */
+
+Route::get('posts', [PostController::class, 'index']);
